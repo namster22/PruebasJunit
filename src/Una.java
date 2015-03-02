e6eda6a604e35e3413ff0a77cb218e107c2531bf
Emilio <Emilio@MSI-GP60>
2015-03-02 09:59:14 +0100

    Calculadora Avanzada

---
+++ src/Una.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Emilio
+ */
+public class Una {
+    static int x[] = {1, 2};
+        int y[];
+
+        void metodoF(int z[]) {z[1]+=2;
+
+        }
+
+        void metodoG() {
+            Una ObjA = new Una();
+            ObjA.x[0]++;
+        }
+
+}
